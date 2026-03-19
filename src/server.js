8ea1bd3abef4a61f9d19ec3e862f4cccb3a336e8
Siddartha Kunwar <siddartha.kunwar01@gmail.com>
2026-03-19 10:38:11 +0545

    db connect, healthCheck logic and route, asynchandler util and user model creation with password hashing

--- src/server.js
+++ src/server.js
@@ -1,11 +1,14 @@
 import dotenv from "dotenv";
 import app from "./app.js";
+import connectDB from "./db/db_connect.js";
 
 dotenv.config({
   path: "./.env",
 });
 
 const port = process.env.PORT || 3000;
+
+connectDB();
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`);
